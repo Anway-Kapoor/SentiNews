b17047acd046988a29d9155d03fa59705d59b921
Anway Kapoor <anwaykapoor2311@gmail.com>
2025-04-04 04:08:02 +0530

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Dashboard from '../components/Dashboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
+      <Dashboard />
+    </main>
+  );
+}
